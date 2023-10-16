d9847bf915e1881395d15e7de041054741297559
monaceKing <leroyjulien055@gmail.com>
2023-10-16 17:11:08 +0000

    initialisation de ma barre de recherche pour trouver un pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemon-list';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`
+  templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  title = 'Akg Family';
+export class AppComponent implements OnInit{
+  pokemonList : Pokemon[] = POKEMONS;
+  pokemonSelected : Pokemon | undefined;
+
+  ngOnInit() {
+   console.table(this.pokemonList);
+  //  this.selectPokemon(this.pokemonList[0]);
+  }
+
+  selectPokemon(pokemonId: string){
+    const id = +pokemonId;
+    const pokemon: Pokemon |undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    if (pokemon) {
+      console.log(`vous avez demandé sur le pokemon ${pokemon.name}`);
+      this.pokemonSelected  = pokemon;
+    } else{
+      console.log(`Le pokémon que vous avez demandé n'existe pas!`);
+      this.pokemonSelected  = pokemon;
+    }
 }
+}
+ 
