d57b8e4062de22d0170def2b0ba543cc839b8b9e
monaceKing <leroyjulien055@gmail.com>
2023-10-17 19:41:16 +0000

    creation des composants list, detail et page d'erreurs, branchementds des routes liées aux pages pour la navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule,Routes } from '@angular/router';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
+const routes: Routes = [
+  {path: 'pokemons', component: ListPokemonComponent},
+  {path: 'pokemon/:id', component: DetailPokemonComponent},
+  {path: '', redirectTo: 'pokemons', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports : [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,23 @@ import { POKEMONS } from './mock-pokemon-list';
   templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit{
-  pokemonList : Pokemon[] = POKEMONS;
-  pokemonSelected : Pokemon | undefined;
+  // pokemonList : Pokemon[] = POKEMONS;
+  // pokemonSelected : Pokemon | undefined;
 
   ngOnInit() {
-   console.table(this.pokemonList);
-  //  this.selectPokemon(this.pokemonList[0]);
+  //  console.table(this.pokemonList);
   }
 
-  selectPokemon(pokemonId: string){
-    const id = +pokemonId;
-    const pokemon: Pokemon |undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
-    if (pokemon) {
-      console.log(`vous avez demandé sur le pokemon ${pokemon.name}`);
-      this.pokemonSelected  = pokemon;
-    } else{
-      console.log(`Le pokémon que vous avez demandé n'existe pas!`);
-      this.pokemonSelected  = pokemon;
-    }
-}
+//   selectPokemon(pokemonId: string){
+//     const id = +pokemonId;
+//     const pokemon: Pokemon |undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+//     if (pokemon) {
+//       console.log(`vous avez demandé sur le pokemon ${pokemon.name}`);
+//       this.pokemonSelected  = pokemon;
+//     } else{
+//       console.log(`Le pokémon que vous avez demandé n'existe pas!`);
+//       this.pokemonSelected  = pokemon;
+//     }
+// }
 }
  
